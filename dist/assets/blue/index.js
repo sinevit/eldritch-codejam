90b64c8e1ec250bfef2d0cf6dec9e5dc1c31e48c
Kris <Krismail>
2022-08-27 10:34:31 +0300

    feat: add gitignore

--- dist/assets/blue/index.js
+++
@@ -1,1 +0,0 @@
-const cards={blue1:"./blue1.png",blue2:"./blue2.png",blue3:"./blue3.png",blue4:"./blue4.png",blue5:"./blue5.png",blue6:"./blue6.png",blue7:"./blue7.png",blue8:"./blue8.png",blue9:"./blue9.png",blue10:"./blue10.png",blue11:"./blue11.png",blue12:"./blue12.png"};export default cards;
